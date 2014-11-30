deed916301e602b896ec75f8d34176114dc71737
John Bohn <jjbohn@gmail.com>
2014-11-29 20:23:12 -0500

    Extract some functions

--- src/phonelib/core.clj
+++ src/phonelib/core.clj
@@ -2,16 +2,35 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.params :refer :all]
             [ring.util.response :refer :all])
   (:import [com.google.i18n.phonenumbers PhoneNumberUtil]))
 
+(defn phone-number-map [number]
+  {:national_number (.getNationalNumber number)
+   :country_code    (.getCountryCode number)
+   :extension       (.getExtension number)
+   })
+
+(defn default-country-code
+  "US")
+
+(defn country-code-from [request]
+  (request :params "country_code" default-country-code))
+
+(defn number-from [request]
+  (request :params "number"))
+
 (defn parse [request]
   (let [util (PhoneNumberUtil/getInstance)]
-    (.parse util "615-521-6937" "US"))
-  (response {:foo 'bar}))
+    (let [number (.parse util (number-from request)
+                         (country-code-from request)))]
+    (response {:numbers [(phone-number-map number)]}))))
 
-(defroutes app
-  (GET "/" [] parse))
+(defroutes app-routes
+  (GET "/api/v1/parse" [] parse))
 
 (def app
-  (wrap-json-response app))
+  (->
+   (wrap-json-response app-routes)
+   (wrap-params)))
